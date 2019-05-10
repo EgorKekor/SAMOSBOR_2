4e9095317ed60418455f414582d33d979e208563
Egor <egor140398@gmail.com>
2019-05-10 21:20:35 +0300

    GameEngine prototype done

--- include/srv/GameEngine.h
+++ include/srv/GameEngine.h
@@ -1,16 +1,24 @@
 #ifndef GAME_ENGINE_H
 #define GAME_ENGINE_H
 #include <SFML/Graphics.hpp>
+#include <../res/message.h>
+#include <../res/secure_queue.h>
+#include <server.h>
+#include <queue>
 
 class GameEngine {
  public:
+    explicit GameEngine();
     int run();
     int handleInput();
     int update(sf::Time deltaTime);
     int quit();
+    bool running();
  private:
     bool running;
-
+    sf::RenderWindow Window;
+    StateManager Manager;
+    server Server;
 
 };
 
