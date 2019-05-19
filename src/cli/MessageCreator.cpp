62ea44ff968365e7a807456ee62b159efa89bb34
Anita <smirnovasiberia@gmail.com>
2019-05-19 20:18:49 +0300

    bool added to protobuf structure

--- src/cli/MessageCreator.cpp
+++ src/cli/MessageCreator.cpp
@@ -6,19 +6,21 @@ MessageCreator::MessageCreator(std::vector<message>& Outp) :
     messOutput(Outp) {}
 
 
-void MessageCreator::SendMouseCommand(sf::Vector2f mouse_position) const {
+void MessageCreator::SendMouseCommand(sf::Vector2f mouse_position, bool isPressed) const {
   message mess;
   mess.set_flag(ClientMessages::flag::MOUSE_PRESSED);
   msg::client_mouse *mouse = mess.add_mouse();
   mouse->set_mouse_x(mouse_position.x);
   mouse->set_mouse_y(mouse_position.y);
+  mouse->set_is_pressed(isPressed);
   messOutput.push_back(mess);
 }
 
-void MessageCreator::SendKeyCommand(size_t key) const {
+void MessageCreator::SendKeyCommand(size_t key, bool isPressed) const {
   message mess;
   mess.set_flag(ClientMessages::flag::KEY_PRESSED);
   msg::client_key *clientKey = mess.add_key();
   clientKey->set_key(key);
+  clientKey->set_is_pressed(isPressed);
   messOutput.push_back(mess);
 }
--- src/cli/Player.cpp
+++ src/cli/Player.cpp
@@ -12,7 +12,7 @@ sf::FloatRect Player::getRect() {
   return sf::FloatRect();
 }
 
-void Player::handle_actor_input(sf::Mouse::Button mouse, bool isPressed) {
+void Player::handle_actor_input(sf::Mouse::Button /*mouse*/, bool /*isPressed*/) {
   if (mainHero) {
 //    sf::Vector2i position = sf::Mouse::getPosition();
 
@@ -22,21 +22,21 @@ void Player::handle_actor_input(sf::Mouse::Button mouse, bool isPressed) {
 void Player::handle_actor_input(sf::Keyboard::Key key, bool isPressed) {
   if (mainHero) {
     if (key == sf::Keyboard::W) {
-      context.messageCreator.SendKeyCommand(ClientMessages::key::W);
+      context.messageCreator.SendKeyCommand(ClientMessages::key::W, isPressed);
     } else if (key == sf::Keyboard::S) {
-      context.messageCreator.SendKeyCommand(ClientMessages::key::S);
+      context.messageCreator.SendKeyCommand(ClientMessages::key::S, isPressed);
     } else if (key == sf::Keyboard::A) {
-      context.messageCreator.SendKeyCommand(ClientMessages::key::A);
+      context.messageCreator.SendKeyCommand(ClientMessages::key::A, isPressed);
     } else if (key == sf::Keyboard::D) {
-      context.messageCreator.SendKeyCommand(ClientMessages::key::D);
+      context.messageCreator.SendKeyCommand(ClientMessages::key::D, isPressed);
     } else if (key == sf::Keyboard::Num1) {
-      context.messageCreator.SendKeyCommand(ClientMessages::key::NUM1);
+      context.messageCreator.SendKeyCommand(ClientMessages::key::NUM1, isPressed);
     } else if (key == sf::Keyboard::Num2) {
-      context.messageCreator.SendKeyCommand(ClientMessages::key::NUM2);
+      context.messageCreator.SendKeyCommand(ClientMessages::key::NUM2, isPressed);
     } else if (key == sf::Keyboard::Num3) {
-      context.messageCreator.SendKeyCommand(ClientMessages::key::NUM3);
+      context.messageCreator.SendKeyCommand(ClientMessages::key::NUM3, isPressed);
     } else if (key == sf::Keyboard::Num4) {
-      context.messageCreator.SendKeyCommand(ClientMessages::key::NUM4);
+      context.messageCreator.SendKeyCommand(ClientMessages::key::NUM4, isPressed);
     }
   }
 }
